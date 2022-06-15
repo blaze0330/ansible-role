08cab4af965434c0cd7e019ca56033365f5751ff
Blaze <blaze0330@gmail.com>
2022-06-15 20:20:45 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {}
+variable "mykey" {}
+variable "mykeypem" {}
+variable "tags" {}
+variable "myami" {
+  description = "in order of; amazon linux 2, redhat enterprise linux 8"
+}
+variable "instancetype" {}
+variable "num" {}
+variable "mysecgr" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
